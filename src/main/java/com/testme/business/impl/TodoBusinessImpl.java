75c19c0c3fce2b6c70732dbdef86d0db1c50ca42
mrdbrg <rdg.brg@gmail.com>
2022-05-08 20:34:15 -0400

    set up project - TodoService and TodoBusinessImpl

---
+++ src/main/java/com/testme/business/impl/TodoBusinessImpl.java
@@ -0,0 +1,52 @@
+package com.testme.business.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.testme.data.api.TodoService;
+
+
+/*
+ * TodoBusinessImpl contains the business logic to be tested.
+ * 
+ * 
+ * TodoBusinessImpl - SUT (System Under Test)
+ * TodoService - DEPENDENCY 
+ * 
+ */
+public class TodoBusinessImpl {
+
+	/*
+	 * TodoService is a dependency of TodoBusinessImpl.
+	 */
+	private TodoService todoService;
+
+	/*
+	 * The constructor enables to test TodoBusinessImpl with any
+	 * implementation of TodoService.
+	 * 
+	 */
+	public TodoBusinessImpl(TodoService todoService) {
+		this.todoService = todoService;
+	}
+	
+	/*
+	 * Method to write tests for.
+	 * 
+	 * The method depends on the TodoService to get the list of the users 
+	 * to operate the logic.
+	 */
+	public List<String> retrieveTodosRelatedToSpring(String user) {
+		List<String> filteredTodos = new ArrayList<String>();
+		List<String> todos = todoService.retrieveTodos(user);
+		
+		for (String todo:todos) {
+			if(todo.contains("Spring")) {
+				filteredTodos.add(todo);
+			}
+		}
+		return filteredTodos;
+	}
+	
+	
+}
---
+++ src/main/java/com/testme/data/api/TodoService.java
@@ -0,0 +1,11 @@
+package com.testme.data.api;
+
+import java.util.List;
+
+/*
+ * TodoService can be communicating to a database or an external 
+ * interface.
+ */
+public class TodoService {
+	public List<String> retrieveTodos(String user);
+}
